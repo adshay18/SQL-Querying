5d43f9cc5370745e5094f221cf4824ab1b3463b0
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2022-01-19 21:03:23 -0500

    finished sql exercise

---
+++ crud.sql
@@ -0,0 +1,10 @@
+-- scratchpad for part 3 of exercise
+
+
+\c products_db;
+
+INSERT INTO products (name, price, can_be_returned) VALUES ('chair', 44.00, false);
+
+INSERT INTO products (name, price, can_be_returned) VALUES ('stool', 25.99, true);
+
+INSERT INTO products (name, price, can_be_returned) VALUES ('table', 124.00, false);
